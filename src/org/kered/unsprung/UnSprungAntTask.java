f238ef4f96d70cd3e20691f1986e9d861958ea4e
Derek Anderson <public@kered.org>
2010-02-10 13:54:36 -0600

    -

--- src/org/kered/unsprung/UnSprungAntTask.java
+++ src/org/kered/unsprung/UnSprungAntTask.java
@@ -1,16 +1,19 @@
 package org.kered.unsprung;
 import java.io.File;
+import java.io.IOException;
 
 import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.DirectoryScanner;
 import org.apache.tools.ant.Task;
 
 
 public class UnSprungAntTask extends Task {
 	
 	private org.apache.tools.ant.types.FileSet src = null;
-	private File dest = null;
+	private File dest = new File("GeneratedSpringContext.java");
+	private File props = null;
 	
-	public void addInner( org.apache.tools.ant.types.FileSet src ) {
+	public void addFileset( org.apache.tools.ant.types.FileSet src ) {
 		this.src = src;
 	}
 	
@@ -18,9 +21,33 @@ public class UnSprungAntTask extends Task {
 		this.dest = dest;
 	}
 
+	public void setProperties(File prop) {
+		this.props = props;
+	}
+
 	@Override
 	public void execute() throws BuildException {
-		System.out.println("woot!");
+
+		UnSprung unSprung = new UnSprung();
+		if( props!=null )
+			try {
+				unSprung.addProperties(props);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		DirectoryScanner ds = src.getDirectoryScanner(getProject());
+		for(String fn : ds.getIncludedFiles()) {
+			File file = new File(fn);
+			try {
+				unSprung.addContextFile(file);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		unSprung.generate(dest);
 	}
 
 
